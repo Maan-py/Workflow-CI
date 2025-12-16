6491f79ccc1857855a416cbe44805735f242258c
Maan-py <mluqmaan22@gmail.com>
2025-12-16 19:45:31 +0700

    first commit

---
+++ MLProject/modelling.py
@@ -0,0 +1,31 @@
+import mlflow
+import mlflow.sklearn
+import pandas as pd
+import dagshub
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+
+mlflow.set_tracking_uri("http://127.0.0.1:5000")
+# dagshub.init(repo_owner="Maan-py", repo_name="membangun-model-msml", mlflow=True)
+mlflow.set_experiment("UNSW_NB15_Basic")
+
+df = pd.read_csv("UNSW_NB15_preprocessing/UNSW_NB15_preprocessed.csv")
+
+X = df.drop(columns=["label"])
+y = df["label"]
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42, stratify=y
+)
+
+with mlflow.start_run():
+    mlflow.sklearn.autolog()
+
+    model = RandomForestClassifier()
+    model.fit(X_train, y_train)
+
+    y_pred = model.predict(X_test)
+
+    acc = accuracy_score(y_test, y_pred)
+    print("Accuracy:", acc)
