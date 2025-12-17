5b8c6ea3516c0550ddc50f16185176f5a7f3476e
Maan-py <mluqmaan22@gmail.com>
2025-12-17 18:45:33 +0700

    fix latest run id

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,15 +1,23 @@
-import os
-import pandas as pd
 import mlflow
 import mlflow.sklearn
+import pandas as pd
+import dagshub
+import os
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
-mlflow.set_tracking_uri("sqlite:///mlflow.db")  # atau sqlite backend
+mlflow.set_tracking_uri("http://127.0.0.1:5000")
+# dagshub.init(repo_owner="Maan-py", repo_name="SMSML_Muhammad-Luqmaan", mlflow=True)
+# mlflow.set_experiment("UNSW_NB15_Basic")
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+mlflow.set_tracking_uri(f"file:{BASE_DIR}/mlruns")
 mlflow.set_experiment("workflow-ci")
 
 df = pd.read_csv("UNSW_NB15_preprocessing/UNSW_NB15_preprocessed.csv")
+
 X = df.drop(columns=["label"])
 y = df["label"]
 
@@ -17,16 +25,19 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42, stratify=y
 )
 
-mlflow.sklearn.autolog()  # autologging tanpa start_run manual
+# with mlflow.start_run():
+mlflow.sklearn.autolog()
 
 model = RandomForestClassifier()
 model.fit(X_train, y_train)
 
 y_pred = model.predict(X_test)
-print("Accuracy:", accuracy_score(y_test, y_pred))
 
-mlflow.sklearn.log_model(model, name="model")
+acc = accuracy_score(y_test, y_pred)
+print("Accuracy:", acc)
 
-active_run = mlflow.active_run()
-if active_run:
-    print("Run ID:", active_run.info.run_id)
+mlflow.sklearn.log_model(
+    sk_model=model,
+    artifact_path="model",
+    registered_model_name=None
+)
