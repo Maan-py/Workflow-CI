92d77d4c2ab73d0304ec479da969cf7b50427d94
Maan-py <mluqmaan22@gmail.com>
2025-12-17 11:26:51 +0700

    refactor(mlflow): use default run name and tracking uri

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -6,7 +6,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
-mlflow.set_tracking_uri("http://127.0.0.1:5000")
+# mlflow.set_tracking_uri("http://127.0.0.1:5000")
 # dagshub.init(repo_owner="Maan-py", repo_name="SMSML_Muhammad-Luqmaan", mlflow=True)
 mlflow.set_experiment("UNSW_NB15_Basic")
 
@@ -19,7 +19,7 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42, stratify=y
 )
 
-with mlflow.start_run(run_name="UNSW_NB15_Basic_Run"):
+with mlflow.start_run():
     mlflow.sklearn.autolog()
 
     model = RandomForestClassifier()
