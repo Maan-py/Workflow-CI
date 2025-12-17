924fa4a29b39cd26ac81e2c4017b454cdeed0598
Maan-py <mluqmaan22@gmail.com>
2025-12-17 11:30:32 +0700

    chore: disable mlflow experiment and run context

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -8,7 +8,7 @@ from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_sc
 
 # mlflow.set_tracking_uri("http://127.0.0.1:5000")
 # dagshub.init(repo_owner="Maan-py", repo_name="SMSML_Muhammad-Luqmaan", mlflow=True)
-mlflow.set_experiment("UNSW_NB15_Basic")
+# mlflow.set_experiment("UNSW_NB15_Basic")
 
 df = pd.read_csv("UNSW_NB15_preprocessing/UNSW_NB15_preprocessed.csv")
 
@@ -19,13 +19,13 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42, stratify=y
 )
 
-with mlflow.start_run():
-    mlflow.sklearn.autolog()
+# with mlflow.start_run():
+mlflow.sklearn.autolog()
 
-    model = RandomForestClassifier()
-    model.fit(X_train, y_train)
+model = RandomForestClassifier()
+model.fit(X_train, y_train)
 
-    y_pred = model.predict(X_test)
+y_pred = model.predict(X_test)
 
-    acc = accuracy_score(y_test, y_pred)
-    print("Accuracy:", acc)
+acc = accuracy_score(y_test, y_pred)
+print("Accuracy:", acc)
